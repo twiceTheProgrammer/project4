73acde6a28b93d73cd7f354575f375e1342cbe6d
twicetheprogrammer <njabulozweli08@gmail.com>
2017-11-27 15:36:27 +0200

    train second upload

--- train-station/js/passenger_scripts.js
+++ train-station/js/passenger_scripts.js
@@ -31,15 +31,7 @@ $(document).ready(function(){
      skin_colour: 'brown',
      age: 22,
      sex : 0
-   },
-   {
-     id:5,
-     name: 'Njabulo',
-     height : 5.8,
-     skin_colour: 'brown',
-     age: 22,
-     sex : 1
-   },
+   }
   ];
   let trains = [
    {
@@ -48,7 +40,7 @@ $(document).ready(function(){
      departure_time: '08:00',
      arrival_time: '09:20',
      stops: 7,
-     capacity: 10,
+     capacity: 2,
      passengers : []
    },
    {
@@ -57,7 +49,7 @@ $(document).ready(function(){
      departure_time: '08:10',
      arrival_time: '09:05',
      stops: 5,
-     capacity:10,
+     capacity:2,
      passengers : []
    }
   ];
@@ -72,7 +64,7 @@ $(document).ready(function(){
           this.passengers = train_data.passengers;
           this.board = function(passenger){
               var passengers_on_train = this.passengers.length ;
-              if ( passengers_on_train >= this.capacity ){
+              if (  passengers_on_train > this.capacity  ){
                     alert('Train is full! wait for the next one...');
                     return false;
               }
@@ -83,6 +75,7 @@ $(document).ready(function(){
                             waiting_passengers.splice(index , 1);
                       }
                   }
+                  this.capacity -= 1;
                   return true;
               }
           }
@@ -107,6 +100,7 @@ $(document).ready(function(){
                 }
                 waiting_passengers.push(unboarding_passenger);
                 unboarded_passengers.push(unboarding_passenger);
+
               }
           }
     }
@@ -121,33 +115,14 @@ let final_northTrain_passengers = [];
 let unboarded_passengers = []
 
 for(let passenger of waiting_passengers){
-      $('#waiting-list').append('<div><br>'+passenger.name+' <button class="btn btn-block btn-dark board-btn" data-passenger-sex="'+passenger.sex+'" data-passenger-id="'+passenger.id+'">Board</button></div>');
+      if (passenger.sex == false ){
+        $('#waiting-list').append('<div><br><i class="fa fa-female" aria-hidden="true"></i>&nbsp; '+passenger.name+' <button class="btn btn-block btn-dark board-btn" data-passenger-sex="'+passenger.sex+'" data-passenger-id="'+passenger.id+'">Board</button></div>');
+      }
+      else {
+        $('#waiting-list').append('<div><br><i class="fa fa-male" aria-hidden="true"></i>&nbsp; '+passenger.name+' <button class="btn btn-block btn-dark board-btn" data-passenger-sex="'+passenger.sex+'" data-passenger-id="'+passenger.id+'">Board</button></div>');
+      }
 }
-
-$('.waiting-list-btn').on('click' , function(){
-  for(let passenger of unboarded_passengers){
-        $('#waiting-list').append('<div><br>'+passenger.name+' <button class="btn btn-block btn-warning board-btn" data-passenger-sex="'+passenger.sex+'" data-passenger-id="'+passenger.id+'">Reboard</button></div>');
-  }
-  $('.board-btn').on('click', function(){
-    let clicked_button = $(this);
-    clicked_button.parent().hide('slow');
-    let passenger_sex = $(this).data('passenger-sex');
-    let passenger_id = $(this).data('passenger-id');
-    let passenger = waiting_passengers.find(function (obj) { return obj.id === passenger_id; });
-
-    if ( passenger_sex == true ){
-        if ( northTrain.board(passenger) == true ){
-            final_northTrain_passengers.push(passenger);
-        }
-    }
-    else {
-        if ( southTrain.board(passenger) == true ){
-          final_southTrain_passengers.push(passenger);
-        }
-    }
-  });
-});
-
+$('.waitingList-total').html('<i class="fa fa-users" aria-hidden="true"></i>&nbsp;Total waiting passengers : ' + waiting_passengers.length );
 $('.board-btn').on('click', function(){
 
       let clicked_button = $(this);
@@ -159,43 +134,46 @@ $('.board-btn').on('click', function(){
       if ( passenger_sex == true ){
           if ( northTrain.board(passenger) == true ){
               final_northTrain_passengers.push(passenger);
+              $('#north-train').append('<div><br><i class="fa fa-male" aria-hidden=true></i>&nbsp; '+passenger.name+' <button class="btn btn-block btn-danger unboard-btn" data-passenger-sex="'+passenger.sex+'" data-passenger-id="'+passenger.id+'">Unboard</button></div>');
           }
+          $('.northTrain-capacity').html('Remaining seats : ' + northTrain.capacity);
+          $('.northTrain-total').html('Onboard passengers : ' + final_northTrain_passengers.length);
       }
       else {
           if ( southTrain.board(passenger) == true ){
             final_southTrain_passengers.push(passenger);
+            $('#south-train').append('<div><br><i class="fa fa-female" aria-hidden=true></i>&nbsp; '+passenger.name+' <button class="btn btn-block btn-danger unboard-btn" data-passenger-sex="'+passenger.sex+'" data-passenger-id="'+passenger.id+'">Unboard</button></div>');
           }
+          $('.southTrain-total').html('Onboard passengers : ' + final_southTrain_passengers.length);
+          $('.southTrain-capacity').html('Seats remaining : ' + southTrain.capacity );
+
       }
+      $('.unboard-btn').on('click' , function(){
+
+              let unboarding_passenger;
+              $(this).parent().fadeOut(1200);
+              let passenger_sex = $(this).data('passenger-sex');
+              let passenger_id = $(this).data('passenger-id');
+
+              if ( passenger_sex == false ){
+                  southTrain.unboard(passenger_id);
+                  unboarding_passenger = unboarded_passengers.find( function(obj){return obj.id === passenger_id});
+                  $('#waiting-list').append('<div><br><i class="fa fa-female" aria-hidden="true"></i>&nbsp; '+unboarding_passenger.name+' <button class="btn btn-block btn-dark board-btn" data-passenger-sex="'+unboarding_passenger.sex+'" data-passenger-id="'+passenger.id+'">Reboard</button></div>');
+                  $('.southTrain-total').html('Onboard passengers : ' + final_southTrain_passengers.length);
+                  $('.southTrain-capacity').html('Seats remaining : ' + southTrain.capacity );
+              }
+              else {
+                  northTrain.unboard(passenger_id);
+                  unboarding_passenger = unboarded_passengers.find( function(obj){return obj.id === passenger_id});
+                  $('#waiting-list').append('<div><br><i class="fa fa-male" aria-hidden="true"></i>&nbsp; '+unboarding_passenger.name+' <button class="btn btn-block btn-dark board-btn" data-passenger-sex="'+unboarding_passenger.sex+'" data-passenger-id="'+passenger.id+'">Reboard</button></div>');
+                  $('.northTrain-capacity').html('Remaining seats : ' + northTrain.capacity);
+                  $('.northTrain-total').html('Onboard passengers : ' + final_northTrain_passengers.length);
+
+              }
+              $('.waitingList-total').html('<i class="fa fa-users" aria-hidden="true"></i>&nbsp;Total waiting passengers : ' + waiting_passengers.length );
+      });
+
+      $('.waitingList-total').html('<i class="fa fa-users" aria-hidden="true"></i>&nbsp;Total waiting passengers : ' + waiting_passengers.length );
 });
 
-      $('.southTrain-passengers').on('click' , function(){
-
-          if ( final_southTrain_passengers.length == false ){
-             $('#south-train').html('No passengers');
-          }
-          else {
-            for (let passenger of final_southTrain_passengers){
-              $('#south-train').append('<div><br>'+passenger.name+' <button class="btn btn-block btn-danger unboard-btn" data-passenger-sex="'+passenger.sex+'" data-passenger-id="'+passenger.id+'">Unboard</button></div>');
-            }
-            $('.unboard-btn').on('click',function(){
-                  southTrain.unboard($(this).data('passenger-id'));
-                  $(this).parent().hide('slow');
-            });
-          }
-      });
-
-      $('.northTrain-passengers').on('click' , function(){
-          if (final_northTrain_passengers.length == false ){
-              $('#north-train').html('No passengers');
-          }
-          else {
-            for (let passenger of final_northTrain_passengers){
-              $('#north-train').append('<div><br>'+passenger.name+' <button class="btn btn-block btn-danger unboard-btn" data-passenger-sex="'+passenger.sex+'" data-passenger-id="'+passenger.id+'">Unboard</button></div>');
-            }
-            $('.unboard-btn').on('click' , function(){
-                  northTrain.unboard($(this).data('passenger-id'));
-                  $(this).parent().hide('slow');
-            });
-          }
-      });
 });
